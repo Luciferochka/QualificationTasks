ebd56631e8d161b4195c5f711a99704af2310a7d
screentolik <miroslav.daniliyk@gmail.com>
2020-10-13 12:08:02 +0300

    Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Tast_1
+{
+    class Привіт_ДонНУ
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Default;
+            string str = "Привіт, ДонНУ!";
+            Console.WriteLine(str);
+        }
+    }
+}
